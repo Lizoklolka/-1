9e9021686ddbaf451814a1390fb81450c54d9c27
LizOk <Lizok.96@bk.ru>
2021-03-05 15:07:30 +0300

    Задание 2

--- Задание 2/main.swift
+++ Задание 2/main.swift
@@ -1,11 +1,19 @@
 //
-//  main.swift
-//  Задание 2
+//  Задание 2.swift
+//  Lesson1
 //
 //  Created by Елизавета Попова on 05.03.2021.
 //
 
 import Foundation
 
-print("Hello, World!")
-
+func calculateTriangle(a: Double, b: Double) {
+    let c: Double = sqrt(b * b + a * a);
+    let P = a + b + c;
+    let S = (a * b)/2;
+    
+    print("Гипотенуза треугольника равна ", c);
+    print("Периметр треугольника равен ", P);
+    print("Площадь треугольника равна ",  S);
+}
+    calculateTriangle(a: 4.00, b: 6.00)
